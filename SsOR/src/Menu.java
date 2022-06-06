703cc5b9ae1f736e2623cd65960bd0b17a12e073
Djulizah <djulizahb74123@gmail.com>
2022-06-07 00:37:58 +0800

    first commit

---
+++ SsOR/src/Menu.java
@@ -0,0 +1,28 @@
+public class Menu {
+    private final int qty;
+    private final String menu;
+    private final int price;
+    
+    Menu (int cqty, String cmenu, int cprice) {
+        this.qty = cqty;
+        this.menu = cmenu;
+        this.price = cprice;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public String getMenu() {
+        return menu;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setText(String string) {
+    }
+
+    
+}
